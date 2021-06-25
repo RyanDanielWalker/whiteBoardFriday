15345f4a6dae013131a747d6453d59537014b54c
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-25 09:20:40 -0700

    Complete two prompts with full notes, prompt one with recursion

---
+++ js/promptOne.js
@@ -0,0 +1,22 @@
+function promptOne(string) {
+  //string = "Jasmine Ann Jones"
+  const stringArray = string.split(" ")
+  // [
+  //   "Jasmine",
+  //   "Ann",
+  //   "Jones"
+  // ]
+  const newString = stringArray.join("%20")
+  // "Jasmine" + "20%" + "Ann" + "20%" + "Jones"
+  return newString
+  // "Jasmine20%Ann20%Jones"
+}
+
+const promptOne = (str) => {
+  const newString = str.split(" ").join("%20")
+  newString
+}
+
+
+//The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array.  The division is done by searching for a pattern; where the pattern is provided as the first parameter in the method's call.  
+//The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
---
+++ js/promptTwo.js
@@ -0,0 +1,29 @@
+function promptTwo(array) {
+  //array = [7, 9, "hi", 12, "hi" 7, 53]
+  let newArray = []
+  array.forEach((element) => {
+    if (!newArray.includes(element)) {
+      newArray.push(element)
+    }
+  })
+  return newArray
+}
+
+//The forEach() method executes a provided function once for each array element.
+//The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
+
+function promptTwoFilter(array) {
+  let newArray = array.filter((element, i) => {
+    return array.indexOf(element) === i
+    //[1, 2, 3, 4, 4, 4, 5]
+    //[1 = i(0), 2 = i(1), 3 = i(2), 4 = i(3), 5 = i(6)]
+  })
+  return newArray
+}
+
+//The indexOf() method returns the index of the first occurrence of an element in an array. 
+//The duplicate element is the element whose index is different from its indexOf() value.
+//To remove the duplicates, you use the filter() method to include only elements whose indexes match their indexOf values.
+//If you wanted only the duplicates, reverse the condition (return array.indexOf(element) !== i)
+
